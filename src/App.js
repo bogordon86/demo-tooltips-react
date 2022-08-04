4494380915824d970a8baabd84b7c64269220bdb
Ben Gordon <bogordon86@gmail.com>
2022-08-03 23:08:13 -0400

    updates to tooltip

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import React from 'react';
 import Tooltip from 'react-bootstrap/Tooltip'
 import logo from './logo.svg';
@@ -11,7 +10,7 @@ import tooltipText from './assets/tooltips.json';
 
 const tooltipArray = Object.values(tooltipText)
 
-export default function App() {
+function App() {
   const renderTT = (which) => {
     var tt = String
     switch(which) {
@@ -23,36 +22,43 @@ export default function App() {
         break;
       default: tt = 'Default tooltip text'
     }
-  return (<Tooltip>{tt}</Tooltip>)
+  return ((<Tooltip>{tt}</Tooltip>))
         } 
   return (
-    <Card className="border border-secondary text-center" style={{ display: 'block', padding: 30 }}>
+    <>
+    <div  style={{display: 'flex', justifyContent: 'center'}}>
       <OverlayTrigger
+        style={{position:'center'}}
         delay={{ hide: 450, show: 300 }}
         overlay={renderTT(1)}
-        placement="top">
-          <h4 style={{fontWeight: 'bold', fontSize: "30"}}>Demo Tooltips</h4>
+        placement="bottom">
+          <h4 style={{mb:50, fontWeight: 'bold', fontSize: "30"}}>Demo Tooltips</h4>
       </OverlayTrigger>
-      <img src={logo} className="App-logo" alt="logo" />
-      <Card>
+    </div>
+      <div  style={{display: 'flex', justifyContent: 'center'}}>  
+        <img src={logo} style={{height: '250px', width: '250px'}} alt="logo" />
+      </div>
       <OverlayTrigger
         delay={{ hide: 450, show: 300 }}
         overlay={renderTT(2)}
         placement="top">
-        <Card>
-          Edit <code>src/App.js</code> and save to reload.
-        </Card>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          Tooltips can be placed in different locations
+        </div>
       </OverlayTrigger>
-      </Card>
-
-      <p>React-Bootstrap Tooltip Component</p>
+      
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <p>React-Bootstrap Tooltip Component</p>
+      </div>
       <OverlayTrigger
         delay={{ hide: 450, show: 300 }}
         overlay={renderTT(3)}
-        placement="bottom"
-      ><Button variant="warning">Tooltip Button</Button>
-      </OverlayTrigger>,
-    </Card>
+        placement="bottom">
+        <div style={{display: 'flex', justifyContent: 'center'}}>  
+          <Button variant="warning">Tooltip Button</Button>
+        </div>
+      </OverlayTrigger>
+    </>
   );
 }
-
+export default App;
